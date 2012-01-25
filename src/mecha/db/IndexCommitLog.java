8522cdb608c0a9f57bf8a3d13798339c495750fe
John Muellerleile <jmuellerleile@gmail.com>
2012-01-24 18:02:40 -0600

    stubbed IndexCommitLog

---
+++ src/mecha/db/IndexCommitLog.java
@@ -0,0 +1,13 @@
+package mecha.db;
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.io.*;
+
+public class IndexCommitLog {
+    
+    public IndexCommitLog() {
+    
+    }
+    
+}
