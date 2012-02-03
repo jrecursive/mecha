1714f0cbcd44d53e8cd1bd78382ba9b39196bdc6
John Muellerleile <jmuellerleile@gmail.com>
2012-02-03 16:20:32 -0600

    Implemented 'eat' and 'limit'.  Moved 'vector-sequencer' to StreamModule.

--- src/mecha/vm/bifs/ETLModule.java
+++ src/mecha/vm/bifs/ETLModule.java
@@ -31,64 +31,7 @@ public class ETLModule extends MVMModule {
     public void moduleUnload() throws Exception {
         log.info("moduleUnload()");
     }
-    
-    /* 
-     * Sequence a vector of results into a stream of 
-     *  ordered data elements according to a simple
-     *  alpha ordering of a specified field.
-     *
-     * See UDVectorSequencer for user-defined ordering
-     *  predicates (using blocks & specified jx.l.vm).
-     *
-    */
-    public class VectorSequencer extends MVMFunction {
-        final String field;
-        final boolean isAscending;
-        final String dataField;
-        final Comparator comparatorFun;
-        final Collator collator;
         
-        public VectorSequencer(String refId, MVMContext ctx, JSONObject config) throws Exception {
-            super(refId, ctx, config);
-            field = config.getString("field");
-            if (config.getString("order").startsWith("asc")) {
-                isAscending = true;
-            } else {
-                isAscending = false;
-            }
-            if (config.has("data-field")) {
-                dataField = config.getString("data-field");
-            } else {
-                dataField = "data";
-            }
-            collator = Collator.getInstance();
-            comparatorFun = new Comparator<Object>() {
-                public int compare(Object obj1, Object obj2) {
-                    try {
-                        String value1 = (String)((LinkedHashMap)obj1).get(field);
-                        String value2 = (String)((LinkedHashMap)obj2).get(field);
-                        if (isAscending) {
-                            return collator.compare(value1, value2);
-                        } else {
-                            return collator.compare(value2, value1);
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                    return 0;
-                }
-            };
-        }
-        
-        public void onDataMessage(JSONObject msg) throws Exception {
-            List<Object> msgVec = msg.getJSONArray(dataField).asList();
-            Collections.<Object>sort(msgVec, comparatorFun);
-            for(Object sortedMsg : msgVec) {
-                broadcastDataMessage(new JSONObject((LinkedHashMap)sortedMsg));
-            }
-        }
-    }
-    
     /*
      * Project a subset of fields.
     */
