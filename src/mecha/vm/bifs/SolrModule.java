89baf4dd6c1bf9dab05151000458a2de0b488dcc
John Muellerleile <jmuellerleile@gmail.com>
2012-02-03 13:19:46 -0600

    Implemented solr-select-iterator (SelectIterator in SolrModule).  Works with with-sorted-coverage.

--- src/mecha/vm/bifs/SolrModule.java
+++ src/mecha/vm/bifs/SolrModule.java
@@ -2,9 +2,12 @@ package mecha.vm.bifs;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
 import java.io.*;
 import java.net.*;
 import java.util.logging.*;
+
 
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.params.ModifiableSolrParams;
@@ -29,6 +32,218 @@ public class SolrModule extends MVMModule {
     public void moduleUnload() throws Exception {
     }
     
+    /*
+     * "Universal" solr-select (param-based)
+     *  Stream implementation.
+    */
+    public class SelectIterator extends MVMFunction {
+        /*
+         * Dedicated iterator thread & control channel communication mechanisms.
+        */
+        final private AtomicBoolean next;
+        final private AtomicBoolean stop;
+        final private ReentrantLock stateLock;
+        final private Thread iteratorThread;
+        final private String iterationLabel;
+    
+        public SelectIterator(String refId, MVMContext ctx, JSONObject config) throws Exception {
+            super(refId, ctx, config);
+            next = new AtomicBoolean(false);
+            stop = new AtomicBoolean(false);
+            stateLock = new ReentrantLock();
+            
+            if (config.has("iterator-name")) {
+                iterationLabel = config.getString("iterator-name");
+            } else {
+                iterationLabel = null;
+            }
+            
+            /*
+             * Dedicated bucket iterator thread.
+            */
+            final Runnable runnableIterator = new Runnable() {
+                public void run() {
+                    try {
+                        JSONObject selectParams = getConfig().getJSONObject("params");
+                        long t_st = System.currentTimeMillis();
+                        long start = 0;
+                        long batchSize = 1000;
+                        long count = 0;
+                        long rowLimit = -1;
+                        long rawFound = 0;
+                        
+                        /*
+                         * Rewrite scored queries into filter queries.
+                        */
+                        if (!selectParams.has("q") &&
+                            !selectParams.has("fq")) {
+                            selectParams.put("q", "*:*");
+                        }
+                        if (selectParams.has("q") &&
+                            !selectParams.has("fq")) {
+                            selectParams.put("fq", selectParams.get("q"));
+                            selectParams.put("q", "*:*");
+                        }
+                        
+                        if (selectParams.has("start")) {
+                            start = Long.parseLong("" + selectParams.get("start"));
+                            selectParams.remove("start");
+                        }
+                        
+                        if (selectParams.has("rows")) {
+                            rowLimit = Long.parseLong("" + selectParams.get("rows"));
+                            selectParams.remove("rows");
+                        }
+
+                        final AtomicBoolean earlyExit = new AtomicBoolean(false);
+                        final JSONObject doneMsg = new JSONObject();
+                        
+                        try {
+                            while(true) {
+                                ModifiableSolrParams solrParams = new ModifiableSolrParams();
+                                for(String k : JSONObject.getNames(selectParams)) {
+                                    solrParams.set(k, "" + selectParams.get(k));
+                                }
+                                solrParams.set("start", "" + start);
+                                solrParams.set("rows", "" + batchSize);
+                                
+                                int batchCount = 0;
+                                QueryResponse res = 
+                                    Mecha.getSolrManager().getIndexServer().query(solrParams);
+                                
+                                /*
+                                 * Document results.
+                                */  
+                                rawFound = res.getResults().getNumFound();
+                                if (res.getResults().getNumFound() == 0) break;
+                                if (rowLimit == -1) {
+                                    rowLimit = res.getResults().getNumFound();
+                                }
+                                for(SolrDocument doc : res.getResults()) {
+                                    try {
+                                        /*
+                                         * Wait for iterator control "next" state.
+                                        */
+                                        while(!next.get()) {
+                                            if (stop.get()) {
+                                                // trigger early exit bubble
+                                                earlyExit.set(true);
+                                                break;
+                                            }
+                                            Thread.sleep(5);
+                                        }
+                                        if (earlyExit.get()) {
+                                            break;
+                                        }
+                                        // bubble early exit up
+                                        stateLock.lock();
+                                        try {
+                                            JSONObject msg = new JSONObject();
+                                            for(String fieldName : doc.getFieldNames()) {
+                                                msg.put(fieldName, doc.get(fieldName));
+                                            }
+                                            broadcastDataMessage(msg);
+                                            count++;
+                                            next.set(false);
+                                        } finally {
+                                            stateLock.unlock();
+                                        }
+                                        batchCount++;
+                                        if (count >= rowLimit) break;
+                                    } catch (Exception ex) {
+                                        ex.printStackTrace();
+                                        try {
+                                            earlyExit.set(true);
+                                            doneMsg.put("iterator-exception",
+                                                Mecha.exceptionToStringArray(ex));
+                                            break;
+                                        } catch (Exception _ex) { _ex.printStackTrace(); break; }
+                                    }
+                                }
+                                start += batchCount;
+                                // bubble early exit up
+                                if (earlyExit.get()) {
+                                    break;
+                                }
+                                if (start >= rowLimit) break;
+                            }
+                            long t_elapsed = System.currentTimeMillis() - t_st;
+                            
+                            doneMsg.put("elapsed", t_elapsed);
+                            doneMsg.put("count", count);
+                            doneMsg.put("found", rawFound);
+                            broadcastDone(doneMsg);
+
+                        } catch (Exception ex) {
+                            log.info("iterator thread exception!");
+                            ex.printStackTrace();
+                            try {
+                                doneMsg.put("exception", Mecha.exceptionToStringArray(ex));
+                            } catch (Exception _ex) { _ex.printStackTrace(); }
+                        }
+                        doneMsg.put("stopped", earlyExit.get());
+                        doneMsg.put("count", count);
+                        broadcastDone(doneMsg);
+                    } catch (Exception ex1) {
+                        ex1.printStackTrace();
+                    }
+                }
+            };
+            iteratorThread = new Thread(runnableIterator);
+        }
+        
+        public void onStartEvent(JSONObject startEventMsg) throws Exception {
+            iteratorThread.start();
+        }
+        
+        public void onCancelEvent(JSONObject msg) throws Exception {
+            stop.set(true);
+            if (iteratorThread.isAlive()) {
+                iteratorThread.interrupt();
+            }
+        }
+        
+        public void onControlMessage(JSONObject msg) throws Exception {
+                        if (!iteratorThread.isAlive()) {
+                log.info("iterator thread has not been started!");
+                return;
+            }
+            
+            final String verb = msg.getString("$");
+            
+            stateLock.lock();
+            try {
+                /*
+                 * "next" - send one record down the pipeline.
+                */
+                if (verb.equals("next")) {
+                    next.set(true);
+                    
+                /*
+                 * "stop" - cancel the iteration.
+                */
+                } else if (verb.equals("stop")) {
+                    stop.set(true);
+                    
+                /*
+                 * unknown
+                */
+                } else {
+                    log.info("Unknown state! msg = " + msg.toString(2));
+                }
+            } finally {
+                stateLock.unlock();
+            }
+        }
+    
+        
+
+    }
+    
+    /*
+     * "Universal" solr-select (param-based)
+     *  Stream implementation.
+    */
     public class Select extends MVMFunction {
         public Select(String refId, MVMContext ctx, JSONObject config) throws Exception {
             super(refId, ctx, config);
