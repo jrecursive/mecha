79a66c062eff7efde5d6a2aa8389393f86d14c03
John Muellerleile <jmuellerleile@gmail.com>
2012-02-28 16:27:37 -0600

    modified mecha CLI to read from config file

--- src/mecha/client/MechaCLI.java
+++ src/mecha/client/MechaCLI.java
@@ -5,17 +5,20 @@ import java.util.*;
 import java.util.logging.*;
 import jline.*;
 
+import mecha.json.JSONObject;
+import mecha.util.TextFile;
+import mecha.util.ConfigParser;
+
 public class MechaCLI {
     final private static Logger log = 
         Logger.getLogger(MechaCLI.class.getName());
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            log.info("usage: MechaCLI <ws://host:port/mecha> <password>");
-        }
-        String host = args[0];
-        int port = Integer.parseInt(args[1]);
-        String pass = args[2];
+        log.info("* reading config.json");
+        JSONObject config = ConfigParser.parseConfig(TextFile.get("config.json"));
+        String host = config.<String>get("server-addr");
+        int port = config.<Integer>get("server-port");
+        String pass = config.<String>get("password");
         
         MechaClient client = new MechaClient(host, port, pass);
         
