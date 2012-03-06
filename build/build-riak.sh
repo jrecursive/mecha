b62da934e660e141893d391c25f1c70bf6bb3b10
John Muellerleile <jmuellerleile@gmail.com>
2012-03-05 17:49:59 -0800

    build changes

--- build/build-riak.sh
+++ build/build-riak.sh
@@ -5,9 +5,17 @@ then
 	tar zxvf riak-1.1.1rc1.tar.gz
 	cp ../src/erlang/riak_kv_mecha_backend.erl riak-1.1.1rc1/deps/riak_kv/src/.
 	cd riak-1.1.1rc1
-		make rel
+	make rel
+if [ ! -d rel/riak ];
+then
+	echo UNABLE TO BUILD RIAK!
+	cd ..
+	./clean-riak.sh
+else
 	cp ../templates/*.template rel/riak/etc/.
 	cd ..
 	ln -s riak-1.1.1rc1/rel/riak riak
 	cd ..
 fi
+fi
+
